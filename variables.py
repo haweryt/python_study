dd4b9a0c30b1e978769e1fc496f11ec11099569d
Dzmitry <antivirys18@gmail.com>
2026-06-16 10:29:29 +0200

    lists

--- variables.py
+++ variables.py
@@ -1,3 +1,5 @@
-a = 5
-b = 3
-print(f"{a} + {b} = {a + b}, {a} - {b} = {a - b}, {a} * {b} = {a * b}, {a} / {b} = {a / b:.2f}")
+numbers = [3, 1, 4, 1, 5, 9, 2, 6]
+numbers_len = len(numbers)
+print(numbers[0])
+print(numbers[numbers_len-1])
+print(numbers_len)
