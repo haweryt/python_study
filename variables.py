9b6ae0c38fe733482a34d8ed20e61c3b0d04da87
Dzmitry <antivirys18@gmail.com>
2026-06-16 10:51:20 +0200

    min() & max()

--- variables.py
+++ variables.py
@@ -1,5 +1,3 @@
 numbers = [3, 1, 4, 1, 5, 9, 2, 6]
-numbers.sort(reverse=True)
-print(f"Max: {numbers[0]}")
-numbers.sort()
-print(f"Min: {numbers[0]}")
+print(f"Max: {max(numbers)}")
+print(f"Min: {min(numbers)}")
